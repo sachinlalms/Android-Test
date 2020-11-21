9bf8c9473b8095601101f7a7c38b4e31bcf47306
sachinlalms <sachinlalms95@gmail.com>
2020-11-21 22:19:28 +0530

    second commit

---
+++ app/src/main/java/com/example/marymathacollege/UploadQuestions.java
@@ -0,0 +1,14 @@
+package com.example.marymathacollege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_questions);
+    }
+}
